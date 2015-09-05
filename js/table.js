04ffd8f76d203b539c0fe81c0486ddaff8e66fc0
ArielRojas <arielwagnerrojas@gmail.com>
2015-09-04 21:13:03 -0400

    date september 2, 2015 autor: Ariel

--- js/table.js
+++ js/table.js
@@ -1,35 +1,42 @@
-/***
- * created 9/1/2015
- *
+/**
+ * Created by ArielWagner on 02/09/2015.
  */
 var Table = function(size){
-    this.size = size;
     this.ships = [];
-    this._field = [];
+    this.board = [];
+    //this._field = [];
+    this._createBoard(size);
     this._createShips();
-    this._initField();
-    this._placeShips();
+    //console.log(this._getCellState(0,1));
+    //this._placeShips();
+};
+
+Table.prototype._createBoard = function(boardSize){
+    this.board = [];
+    for(var row = 0; row < boardSize; row++){
+        var rowCollection = [];
+        for(var col = 0; col < boardSize; col++){
+            rowCollection[col] = "0";
+        }
+        this.board[row] = rowCollection;
+    }
 };
 Table.prototype._createShips = function(){
-    var numShips = 3;
-    for(var i = 0; i< numShips; i++){
-        var ship = new Ship(i, 3);
-        this.ships.push(ship);
-    }
+    this.ships = {
+        0:{name:"Aircraft carrier", size:5, hits:0, direction:"Vertical", coordinates:[[0,0],[0,0],[0,0],[0,0],[0,0]]},
+        1:{name:"Battleship", size:4, hits:0, direction:"Vertical", coordinates:[[0,0],[0,0],[0,0],[0,0]]},
+        2:{name:"Submarine", size:3, hits:0, direction:"Vertical", coordinates:[[0,0],[0,0],[0,0]]},
+        3:{name:"Destroyer", size:3, hits:0, direction:"Vertical", coordinates:[[0,0],[0,0],[0,0]]},
+        4:{name:"Patrol boat", size:2, hits:0, direction:"Vertical", coordinates:[[0,0],[0,0],[0,0]]}
+    };
+    this.shipCount = 5;
 };
+Table.prototype._getCellState = function(x, y){
 
-Table.prototype._initField = function(){
-    for(var i = 0; i< this.size; i++) {
-        this._field.push('-');
-    }
-};
-
-Table.prototype._placeShips = function(){
-    for(var i = 0; i < this.ships.length; i++){
-        var ship = this.ships[i];
-        var initPos = parseInt(Math.random() * (this.size - ship.size));
-        for(j = initPos; j< (initPos + ship.size); j++){
-            this._field[j] = ship.id;
-        }
-    }
+    if(this.board[x][y] == "2")
+        return "Hit";
+    else if(this.board[x][y] == "1")
+        return "Miss";
+    else if(this.board[x][y] == "0")
+        return "Unknown";
 };
