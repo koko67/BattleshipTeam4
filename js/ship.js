7baad83ddd585b2c2b370f1b204157225c631645
josecardozo <jose.cardozo@fundacion-jala.org>
2015-09-12 13:06:43 -0400

    Update the table class

--- js/ship.js
+++ js/ship.js
@@ -1,4 +1,9 @@
-var Ship = function(id, size){
-	this.id = id;
-	this.size = size;
+var Ship = function(){
+	//this.id = id;
+	this.size;
+	this.name;
+	this.hits;
+	this.direction;
+	this.coordinates=[];
+
 };
